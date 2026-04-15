9eb59164e8ba62b20c60c4250e33f745b5ae6906
SoyKhaler <aaronstone2000@gmail.com>
2026-04-15 18:45:58 +0200

    new

--- script.js
+++
@@ -1,9 +0,0 @@
-window.onload = function() {
-    document.body.style.opacity = 1;
-};
-
-document.addEventListener('scroll', function() {
-    var movingBackground = document.querySelector('.moving-background');
-    var scrollPosition = window.pageYOffset;
-    movingBackground.style.transform = 'translateY(' + scrollPosition * 0.1 + 'px)';
-});
